5b22fc430a62378dfb2a5354780b52e4991b84ef
Nelson Huamán H. <libra.nelsonhuaman@gmail.com>
2024-11-17 21:31:00 -0500

    Vite + Vue

---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+   <h1 class="text-4xl font-extrabold text-gray-500 text-center">
+      ¿Cuánto <span class="text-indigo-600">dinero</span> necesitas? - Actualizado
+   </h1>
+</template>
